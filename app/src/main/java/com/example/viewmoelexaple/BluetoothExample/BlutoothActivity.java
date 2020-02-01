281c64dbfd110dd95a928d6e583877ecc68f6d46
nateshmailme@gmail.com <nateshmaile@gmail.com>
2020-02-01 22:28:16 +0530

    changebluttoth

--- app/src/main/java/com/example/viewmoelexaple/BluetoothExample/BlutoothActivity.java
+++ app/src/main/java/com/example/viewmoelexaple/BluetoothExample/BlutoothActivity.java
@@ -11,6 +11,6 @@ public class BlutoothActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recycler);
+        setContentView(R.layout.activity_bluetooth);
     }
 }
